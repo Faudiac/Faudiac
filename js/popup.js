fab5d640181966ccde5d60208b3514dc695a1114
Faudiac <115066002+Faudiac@users.noreply.github.com>
2023-09-10 17:10:44 +0500

    ywes

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+function READINFO() {
+    document.getElementById("INFOPOP").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("INFOPOP").style.display = "none";
+}
